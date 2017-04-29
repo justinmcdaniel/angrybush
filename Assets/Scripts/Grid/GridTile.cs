c728c875161bb6a5ae97398ee52780d83f2fef54
Justin McDaniel <justin.mcdaniel@Justins-MacBook-Pro.local>
2017-04-28 23:10:07 -0500

    Built basic grid. Got mouse enter and exit events working to relay current mouse grid positions to default MouseController object/script.

---
+++ Assets/Scripts/Grid/GridTile.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridTile : MonoBehaviour {
+
+	public int gridX;
+	public int gridY;
+
+	public MouseController mouseController;
+
+	// Use this for initialization
+	void Awake () {
+		mouseController = (MouseController)GameObject.Find ("MouseController").GetComponent (typeof(MouseController));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseEnter() {
+		mouseController.gridX = this.gridX;
+		mouseController.gridY = this.gridY;
+		Debug.Log ("X: " + this.gridX + ", Y: " + this.gridY);
+	}
+	void OnMouseExit() {
+		mouseController.gridX = -1;
+		mouseController.gridY = -1;
+	}
+
+	//Move to character
+	void OnMouseUp() {
+		mouseController.mouseIsDown = false;
+	}
+
+	void OnMouseDown() {
+		mouseController.mouseIsDown = true;
+	}
+}
