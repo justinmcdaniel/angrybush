c5af9bff54e11b999ff1dcceb0cf07f9c0c8271a
LC Wadle <lcwadle@gmail.com>
2017-04-29 10:39:40 -0500

    Added Stage script to Main Camera. - Added Dictionaries for Plants and Pollution positions.

---
+++ Assets/Scripts/Stage.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Stage : MonoBehaviour {
+
+	public GameObject plant_1;
+	public string plant_1_position;
+	public GameObject plant_2;
+	public string plant_2_position;
+	public Dictionary <string, GameObject> plants;
+
+	public GameObject pollution_1;
+	public string pollution_1_position;
+	public Dictionary <string, GameObject> pollutions;
+
+	// Use this for initialization
+	void Awake () {
+		plants = new Dictionary<string, GameObject>() {
+			{plant_1_position, plant_1},
+			{plant_2_position, plant_2}
+		};
+		pollutions = new Dictionary<string, GameObject>() {
+			{pollution_1_position, pollution_1}
+		};
+	}
+}
