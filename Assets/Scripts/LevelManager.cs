10ae5de09279d3860ca40e5822773490a672879f
LC Wadle <lcwadle@gmail.com>
2017-04-30 11:34:41 -0500

    UI stuff

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -573,6 +573,9 @@ public class LevelManager : MonoBehaviour {
 	}
 
 	void levelWin () {
+		foreach (GameObject plantObject in gameManager.plants) {
+			plantObject.SetActive (false);
+		}
 		SceneManager.LoadScene ("LevelWin");
 	}
 
