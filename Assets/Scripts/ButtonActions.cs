869204350cf98ef117555ba9110047f13e267ed9
LC Wadle <lcwadle@gmail.com>
2017-05-01 18:19:03 -0500

    Added mainMenu, campaignMenu, and introScene. Fixed navigation through scenes including Win and Lose scenes. Moved GameManager, and globalStats to mainMenu scene. Removed mouseController from start scene.

---
+++ Assets/Scripts/ButtonActions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneChanges : MonoBehaviour {
+
+	public void gotoIntroScene() {
+		SceneManager.LoadScene ("Intro");
+	}
+
+	public void gotoMenuScene() {
+		SceneManager.LoadScene ("LevelSelection");
+	}
+
+	public void gotoLevel1() {
+		SceneManager.LoadScene ("Level1");
+	}
+}
---
+++ Assets/Scripts/SceneSelection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneSelection : MonoBehaviour {
+
+	public void gotoIntroScene() {
+		SceneManager.LoadScene ("Intro");
+	}
+
+	public void gotoMenuScene() {
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+	public void gotoLevel1() {
+		SceneManager.LoadScene ("Level1");
+	}
+
+	public void gotoCampaign() {
+		SceneManager.LoadScene ("Campaign");
+	}
+}
